519070fdda819ace37e2fa061a3e9958db0d2dd9
sudheer <mindq.selenium2016@gmail.com>
2019-07-22 11:54:42 +0530

    logging implemented

--- src/test/java/com/OHRMS/testcasescripts/LoginPageTest.java
+++ src/test/java/com/OHRMS/testcasescripts/LoginPageTest.java
@@ -2,6 +2,7 @@ package com.OHRMS.testcasescripts;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -14,6 +15,7 @@ import com.OHRMS.Pages.Dashboardpage;
 import com.OHRMS.Pages.LoginPage;
 import com.OHRMS.baseclass.Baseclass;
 import com.OHRMS.util.Testutil;
+import com.mongodb.diagnostics.logging.Loggers;
 
 //import Academy.HomePage;
 
@@ -23,6 +25,10 @@ public class LoginPageTest extends Baseclass {
 	com.OHRMS.Pages.HomePage hp;
 	LoginPage lp;
 	private String loginpageimg;
+	
+	// logger class declaration on current test case class name
+Logger logger=Logger.getLogger(LoginPageTest.class);
+	String configurepath=System.getProperty("C:\\Users\\sudheer\\git\\Mayselenium1030am\\OHRMS\\src\\main\\java\\log4j.properties");
    
 	public LoginPageTest() throws IOException {
 		super();
@@ -31,7 +37,7 @@ public class LoginPageTest extends Baseclass {
 
 	@BeforeTest
 	public void setup() throws Exception{
-		
+		logger.info("initialization method started");
 		initialization();
 		lp=new LoginPage();	//login page initialization
 		
@@ -39,6 +45,7 @@ public class LoginPageTest extends Baseclass {
 	
 	@Test(priority=1)
 	public void loginPageTitleTest(){
+		logger.info("loginpagetitlevalidation started");
 		String title=lp.validateloginPageTitle();
 		Assert.assertEquals(title, "OrangeHRM");
 		
@@ -49,7 +56,9 @@ public class LoginPageTest extends Baseclass {
 	public void LoginTest() throws Exception{
 		
 		takescreenshot("loginpageimg");
+		logger.info("screenshot captured");
 		hp=lp.login(prop.getProperty("username"),prop.getProperty("password"));
+		logger.info("login testcase completed");
 	}
 	
 
